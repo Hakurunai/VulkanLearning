01621459f8fb3352b94ee07f3faeb22b988810e5
Hakurunai <guillaumeal.pro@gmail.com>
2024-10-01 12:03:59 +0200

    Some modification : - $(Plateform) is removed from OutputDir - Ressources is added near the .sln, one rank before the .exe - .spv shaders are placed inside this Ressources directory - compileShaders.bat now redirect his output to Ressources

--- VKLearning/VKLearning/Sources/Shaders/compileShaders.bat
+++ VKLearning/VKLearning/Sources/Shaders/compileShaders.bat
@@ -1,6 +1,10 @@
 REM chemin relatif au dossier contenant glslc.exe
 set GLSLC_PATH=%~dp0..\..\..\libs\VulkanSDK\1.3.290.0\Bin\
 
-%GLSLC_PATH%glslc.exe simple_shader.vert -o simple_shader.vert.spv
-%GLSLC_PATH%glslc.exe simple_shader.frag -o simple_shader.frag.spv
+REM path to the ressources folder used by our program
+set OUTPUT_DIR=%~dp0..\..\..\Ressources\Shaders\
+if not exist "%OUTPUT_DIR%" ( mkdir "%OUTPUT_DIR%" )
+
+%GLSLC_PATH%glslc.exe simple_shader.vert -o %OUTPUT_DIR%simple_shader.vert.spv
+%GLSLC_PATH%glslc.exe simple_shader.frag -o %OUTPUT_DIR%simple_shader.frag.spv
 REM pause
